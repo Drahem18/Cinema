4676df417ff8104e6bbe9665853c31b6cbb78e69
Drahem18 <youssefdrahem1@gmail.com>
2024-05-26 03:18:20 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+import Header from "./pages/Header";
+import Banner from "./pages/Banner";
+import Main from "./pages/Main";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "swiper/css";
+import BackToTopBtn from "./components/BackToTopBtn";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Banner />
+      <Main />
+      <BackToTopBtn />
+    </>
+  );
+}
+
+export default App;
